ef5851504790248021b7302724d8aee98ce2a3b3
MaggieShpileva <shpileva.rita@gmail.com>
2023-04-28 09:21:35 +0300

    update confige

--- next.config.js
+++ next.config.js
@@ -9,35 +9,6 @@ const nextConfig = {
   images: {
     domains: ["cdn.iz.ru"],
   },
-  i18n: {
-    locales: ["en", "ru"],
-    defaultLocale: "ru",
-  },
-  async getStaticPaths() {
-    const { locales } = i18n;
-
-    const paths = locales.flatMap((locale) => {
-      return [
-        {
-          params: {
-            locale,
-          },
-        },
-      ];
-    });
-
-    return {
-      paths,
-      fallback: false,
-    };
-  },
-  async getStaticProps({ params }) {
-    return {
-      props: {
-        locale: params.locale,
-      },
-    };
-  },
 };
 
 module.exports = nextConfig;

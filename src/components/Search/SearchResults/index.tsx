f3135b13c602eadc686503e398a424e1acf2adeb
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-04-29 16:06:11 +0700

    search results

---
+++ src/components/Search/SearchResults/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import styles from './index.module.scss';
+import { RiUser4Line } from 'react-icons/ri';
+
+type SearchResultsProps = {
+  className?: string;
+  placeholder?: string
+  results: Array<any>
+}
+
+const SearchResults: FC<SearchResultsProps> = ({ className, placeholder, results }) => {
+
+  return (
+    <ul className={[styles.searchResults, className].join(' ')}>
+      {results.map((item, i) => (
+        <li key={i} className={styles.list}>
+          <RiUser4Line className={styles.icon} />
+          <div>
+            <div className={styles.item}>{item}</div>
+            <div className={styles.label}>{placeholder}</div>
+          </div>
+        </li>
+      ))}
+    </ul>)
+};
+
+export default SearchResults;

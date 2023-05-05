029a3ed70c2c8a7a140d4f4734aaa525240f8c6a
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-05-05 18:47:53 +0700

    add store, types

---
+++ src/store/reducers/MoviesSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const moviesSlice = createSlice({
+  name: 'movies',
+  initialState,
+  reducers: {},
+});
+
+//export const {  } = moviesSlice.actions;
+export default moviesSlice.reducer;
---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import moviesReducer from './reducers/MoviesSlice';
+
+export const store = configureStore({
+  reducer: { moviesReducer },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;

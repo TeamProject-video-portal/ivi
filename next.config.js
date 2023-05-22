2f05bad97ee9e626622e6f3dd302a6738a8919aa
MaggieShpileva <shpileva.rita@gmail.com>
2023-05-22 13:02:53 +0300

    remove locales

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["ru", "en"],
-    defaultLocale: "ru",
-  },
+  // i18n: {
+  //   locales: ["ru", "en"],
+  //   defaultLocale: "ru",
+  // },
 
   locales: ["en", "ru"],
   defaultLocale: "ru",

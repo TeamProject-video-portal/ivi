cfd8c18602cff45ccc7fe3e24fe27a1cadf88a38
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-05-26 23:11:05 +0700

    correct path api

--- src/Redux/persons/worker.ts
+++ src/Redux/persons/worker.ts
@@ -1,8 +1,8 @@
 import { IPerson } from "@/types/types";
 
 export const personsApi = async () => {
-  //const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/person`);
-  const response = await fetch(`http://localhost:3000/api/person`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/person`);
+  //const response = await fetch(`http://localhost:3000/api/person`);
   const dataPersons = (await response.json()) as IPerson[];
   return dataPersons;
 };

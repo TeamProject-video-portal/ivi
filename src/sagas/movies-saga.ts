1038e2b6f49782c5a8112e293fa13dce8c8bc558
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-05-26 11:45:16 +0700

    correct movies saga

---
+++ src/sagas/movies-saga.ts
@@ -0,0 +1,14 @@
+import { call, put } from "redux-saga/effects";
+import { getMovies, getMoviesError } from "@/Redux/movies/actions";
+import { IMovie } from "@/types/types";
+import { movieApi } from "@/Redux/movies/worker";
+
+export function* getMoviesSaga() {
+  try {
+    const response: IMovie[] = yield call(movieApi);
+    yield put(getMovies(response));
+  } catch (error) {
+    console.log("error in getMoviesSaga", error);
+    // yield put(getMoviesError(String(error)));
+  }
+}

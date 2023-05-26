01040bcca1e1626e2aca201bf36c102d23d8158b
MaggieShpileva <shpileva.rita@gmail.com>
2023-05-26 09:06:36 +0300

    add local data for top movie

--- src/Redux/topTenMovies/worker.ts
+++ src/Redux/topTenMovies/worker.ts
@@ -1,20 +1,26 @@
-import { BannerType } from "@/types/types";
-import main_banner from "@/data/Main_banner.json";
+import { BannerType, ResponseTopMovieKPType } from "@/types/types";
+import topMovie from "@/data/top_movie.json";
 import axios from "axios";
+import { TopMovieReducerT } from "./reducer";
 
 export const getDataTopMoviesWorker = async () => {
-  const res = await axios({
-    method: "get",
-    url: "https://api.kinopoisk.dev/v1.3/movie",
-    headers: {
-      "Content-Type": "application/json",
-      "X-API-KEY": `13RH6Q2-2T1M1E7-M50R852-366EP7D`,
-    },
-    params: {
-      Application: "13RH6Q2-2T1M1E7-M50R852-366EP7D",
-      top10: "!null",
-      selectFields: "id poster",
-    },
+  // const res = await axios({
+  //   method: "get",
+  //   url: "https://api.kinopoisk.dev/v1.3/movie",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "X-API-KEY": `13RH6Q2-2T1M1E7-M50R852-366EP7D`,
+  //   },
+  //   params: {
+  //     Application: "13RH6Q2-2T1M1E7-M50R852-366EP7D",
+  //     top10: "!null",
+  //     selectFields: "id poster",
+  //   },
+  // });
+  // return res.data.docs;
+  return new Promise<ResponseTopMovieKPType[]>((resolve, reject) => {
+    setTimeout(() => {
+      resolve(topMovie.data);
+    }, 1000);
   });
-  return res.data.docs;
 };

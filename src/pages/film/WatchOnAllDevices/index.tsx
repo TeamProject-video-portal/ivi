f4e6de4e178f1e6b6491ac57c4d1247c57dea8ff
MaggieShpileva <shpileva.rita@gmail.com>
2023-05-28 01:19:27 +0300

    add content on movie page

--- src/pages/film/WatchOnAllDevices/index.tsx
+++ src/pages/film/WatchOnAllDevices/index.tsx
@@ -3,15 +3,13 @@ import styles from "./index.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import { useTranslation } from "next-export-i18n";
-import FilmButtons from "../FilmButtons";
 import { FilmLangType } from "@/types/types";
-import { useRouter } from "next/router";
 import tvBg from "../../../images/tv-without-poster.png";
-import ipadBG from "@/images/ipad-without-poster.png";
+import ipadBG from "../../../images/ipad-without-poster.png";
 import { Button } from "@/components/Button/Button";
 type Props = {
-  filmPicture: string;
-  filmLang: FilmLangType[];
+  filmPicture?: string;
+  filmLang?: FilmLangType[];
 };
 
 export const WatchOnAllDevices: FC<Props> = (props) => {
@@ -19,7 +17,10 @@ export const WatchOnAllDevices: FC<Props> = (props) => {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
-        <h2>Cмотреть {props.filmLang[0].filmName} на всех устройствах</h2>
+        <h2>
+          Cмотреть {props.filmLang && props.filmLang[0].filmName} на всех
+          устройствах
+        </h2>
         <p>
           Приложение доступно для скачивания на iOS, Android, SmartTV и
           приставках
@@ -38,13 +39,15 @@ export const WatchOnAllDevices: FC<Props> = (props) => {
             width={600}
             className={styles.tv_bg_img}
           ></Image>
-          <Image
-            src={props.filmPicture}
-            alt=""
-            width={600}
-            height={600}
-            className={styles.tv_main_img}
-          ></Image>
+          {props.filmPicture && (
+            <Image
+              src={props.filmPicture}
+              alt=""
+              width={600}
+              height={600}
+              className={styles.tv_main_img}
+            ></Image>
+          )}
         </div>
         <div className={styles.ipad}>
           <Image
@@ -53,13 +56,15 @@ export const WatchOnAllDevices: FC<Props> = (props) => {
             width={500}
             className={styles.ipad_bg_img}
           ></Image>
-          <Image
-            src={props.filmPicture}
-            alt=""
-            width={600}
-            height={600}
-            className={styles.ipad_main_img}
-          ></Image>
+          {props.filmPicture && (
+            <Image
+              src={props.filmPicture}
+              alt=""
+              width={600}
+              height={600}
+              className={styles.ipad_main_img}
+            ></Image>
+          )}
         </div>
       </div>
     </div>

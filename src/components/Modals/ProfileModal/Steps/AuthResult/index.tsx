297760859041d01afb2b5bf7883150088e97c675
MaggieShpileva <shpileva.rita@gmail.com>
2023-06-02 00:56:23 +0300

    add registration

--- src/components/Modals/ProfileModal/Steps/AuthResult/index.tsx
+++ src/components/Modals/ProfileModal/Steps/AuthResult/index.tsx
@@ -1,31 +1,70 @@
-import { FC, Dispatch, SetStateAction, useRef, useState } from "react";
-import Link from "next/link";
+import { FC, Dispatch, SetStateAction, useState, useEffect } from "react";
 import styles from "../index.module.scss";
 import { Button } from "@/components/Button/Button";
 import CompanyPolicy from "../../PolicyBlock";
+import { SignInResponse } from "next-auth/react";
 
 type Props = {
   openModal: boolean;
   setOpenModal: Dispatch<SetStateAction<boolean>>;
+  result: SignInResponse | undefined;
+  isRegistration: boolean;
+  setIsRegistration: Dispatch<SetStateAction<boolean>>;
+  isAuthorization: boolean;
+  setIsAuthorization: Dispatch<SetStateAction<boolean>>;
+  isNewAccout: boolean;
 };
 const AuthResult: FC<Props> = (props) => {
+  const [title, setTitle] = useState<string>();
+  const authHandleClick = () => {
+    props.setOpenModal(false);
+    props.setIsRegistration(false);
+  };
+
+  useEffect(() => {
+    console.log(props.result);
+    if (props.result?.ok) {
+      setTitle("Вы успешно авторизованы");
+    } else if (!props.result?.ok && props.isNewAccout) {
+      setTitle("Вы успешно зарегистрированы");
+    } else if (!props.result?.ok) {
+      setTitle("Логин или пароль введены  неправильно");
+    }
+  }, [props.result]);
+
   return (
     <div className={styles.content}>
       <div className={styles.message}>
-        <div className={styles.subtitle}>
-          Вы успешно авторизованы
-          {/* : "Ошибка: логин или пароль введены  неправильно"*/}
-        </div>
+        <div className={styles.subtitle}>{title}</div>
       </div>
       <div className={styles.enter_data}>
         <div className={styles.continue}>
-          <Button
-            className={`${styles.continue__btn} ${styles.activeContinue} `}
-            color="pink"
-            onClick={() => props.setOpenModal(false)}
-          >
-            Продолжить
-          </Button>
+          {props.result?.ok || props.isNewAccout ? (
+            <Button
+              className={`${styles.continue_btn} ${styles.activeContinue} `}
+              color="pink"
+              onClick={authHandleClick}
+            >
+              Продолжить
+            </Button>
+          ) : (
+            <div>
+              <Button
+                className={`${styles.continue_btn} ${styles.activeContinue} `}
+                color="pink"
+                onClick={() => props.setIsRegistration(true)}
+              >
+                Зарегистрироваться
+              </Button>
+              <Button
+                className={`${styles.continue_btn} ${styles.activeContinue} `}
+                color="pink"
+                onClick={() => props.setIsAuthorization(true)}
+              >
+                Войти
+              </Button>
+            </div>
+          )}
         </div>
         <CompanyPolicy />
       </div>
--- src/components/Modals/ProfileModal/index.tsx
+++ src/components/Modals/ProfileModal/index.tsx
@@ -1,29 +1,63 @@
 import { FC, Dispatch, SetStateAction, useState, useEffect } from "react";
 import styles from "./index.module.scss";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, SignInResponse, useSession } from "next-auth/react";
 import AuthSteps from "./Steps/AuthSteps.tsx";
 import AuthResult from "./Steps/AuthResult";
+import { CSSTransition } from "react-transition-group";
 
 type ProfileModalProps = {
   openModal: boolean;
   setOpenModal: Dispatch<SetStateAction<boolean>>;
 };
 
+export type DataUser = {
+  nickName: string;
+  login: string;
+  password: string;
+};
+
+const contentAuth: Record<number, string[]> = {
+  0: ["Войдите или зарегистрируйтесь", "Через email"],
+  1: ["Введите пароль для регистрации или авторизации", "Пароль"],
+};
+
+const contentRegistraion: Record<number, string[]> = {
+  0: ["Введите почту ", "Почта"],
+  1: ["Введите никнейм", "Никнейм"],
+  2: ["Введите пароль ", "Пароль"],
+};
+
 const ProfileModal: FC<ProfileModalProps> = ({ openModal, setOpenModal }) => {
-  const [login, setLogin] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const [step, setStep] = useState<number>(0);
   const [inputData, setInputData] = useState("");
   const { data: session } = useSession();
+  const [result, setResult] = useState<SignInResponse | undefined>();
+  const [isRegistration, setIsRegistration] = useState<boolean>(false);
+  const [isAuthorization, setIsAuthorization] = useState<boolean>(false);
+  const [dataUser, setDataUser] = useState<DataUser>({
+    nickName: "",
+    login: "",
+    password: "",
+  });
+  const [endStep, setEndStep] = useState(2);
+  const [enterData, setEnterData] = useState(contentAuth);
+  const [isNewAccout, setIsNewAccout] = useState(false);
 
+  //авторизация пользователя
   const authorization = async () => {
-    console.log("login", login, "password", password);
-    const payload = { username: `${login}`, password: `${password}` };
+    const payload = {
+      username: `${dataUser.login}`,
+      password: `${dataUser.password}`,
+    };
     const res = await signIn("credentials", {
       ...payload,
       redirect: false,
     });
-    console.log(res);
+    setResult(res);
+  };
+
+  const registration = () => {
+    setIsNewAccout(true);
   };
 
   const googleInputHandler = () => {
@@ -36,18 +70,77 @@ const ProfileModal: FC<ProfileModalProps> = ({ openModal, setOpenModal }) => {
   };
 
   useEffect(() => {
-    if (step === 1) {
-      setLogin(inputData);
-    } else if (step === 2) {
-      setPassword(inputData);
+    if (isAuthorization) {
+      setIsRegistration(false);
+      setStep(0);
+    }
+    console.log(isAuthorization);
+  }, [isAuthorization]);
+
+  //запись введенных данных
+  useEffect(() => {
+    if (!isRegistration) {
+      if (step === 1) {
+        setDataUser({ nickName: "", login: inputData, password: "" });
+        setInputData("");
+      } else if (step === 2) {
+        setDataUser({
+          nickName: "",
+          login: dataUser?.login,
+          password: inputData,
+        });
+        setInputData("");
+        setIsAuthorization(false);
+      }
+    } else {
+      if (step === 1) {
+        setDataUser({ nickName: "", login: inputData, password: "" });
+      } else if (step === 2) {
+        setDataUser({
+          nickName: inputData,
+          login: dataUser?.login,
+          password: "",
+        });
+      } else if (step === 3) {
+        setDataUser({
+          nickName: dataUser.nickName,
+          login: dataUser?.login,
+          password: inputData,
+        });
+      }
     }
   }, [step]);
 
   useEffect(() => {
-    if (login.length !== 0 && password.length !== 0) {
+    if (dataUser.nickName === "" && dataUser.password !== "") {
       authorization();
+    } else if (
+      dataUser.nickName !== "" &&
+      dataUser.login !== "" &&
+      dataUser.password !== ""
+    ) {
+      ////////////////////////
+      //запрос на регистрацию
+      /////////////////
+      authorization();
+      registration();
+      console.log("need registration");
     }
-  }, [login, password]);
+  }, [dataUser]);
+
+  //при регистрации введенные ранее данные обнуляются и последующие поля отображаются с другими данными для ввода
+  useEffect(() => {
+    if (isRegistration) {
+      setDataUser({
+        nickName: "",
+        login: "",
+        password: "",
+      });
+      setStep(0);
+      setEndStep(3);
+      setEnterData(contentRegistraion);
+    }
+  }, [isRegistration]);
 
   return (
     <div
@@ -62,21 +155,32 @@ const ProfileModal: FC<ProfileModalProps> = ({ openModal, setOpenModal }) => {
         </button>
         <div className={styles.bar}>
           <div
-            className={styles.bar__line}
+            className={styles.bar_line}
             style={{ width: `calc(33.33% * ${step})` }}
           ></div>
         </div>
       </div>
-      {step < 2 && (
+
+      {step < endStep && (
         <AuthSteps
           step={step}
           setStep={setStep}
           inputData={inputData}
           setInputData={setInputData}
+          content={enterData}
         />
       )}
-      {session?.user.name && (
-        <AuthResult openModal={openModal} setOpenModal={setOpenModal} />
+      {step === endStep && (
+        <AuthResult
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          result={result}
+          isRegistration={isRegistration}
+          setIsRegistration={setIsRegistration}
+          isAuthorization={isAuthorization}
+          setIsAuthorization={setIsAuthorization}
+          isNewAccout={isNewAccout}
+        />
       )}
     </div>
   );

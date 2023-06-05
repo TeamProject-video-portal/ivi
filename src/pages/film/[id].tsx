cf01514d6f736643a66c351161ff0155e6858ae8
MaggieShpileva <shpileva.rita@gmail.com>
2023-06-06 00:53:56 +0300

    remove errors

--- src/pages/film/[id].tsx
+++ src/pages/film/[id].tsx
@@ -15,6 +15,7 @@ import ActorsSlider from "@/components/Sliders/ActorsSlider";
 import { TrailerModal } from "@/components/Modals/TrailerModal";
 import WatchOnAllDevices from "./WatchOnAllDevices";
 import InfoMovie from "./InfoMovie";
+import { getMovieWorker } from "@/Redux/movie/workers";
 
 const breadcrumbs: Breadcrumb[] = [
   { item: "Фильмы", path: "/movies" },
@@ -96,15 +97,13 @@ export const getStaticProps: GetStaticProps = async (context) => {
   };
 };
 
-export const getStaticPaths = async (context: number) => {
-  // const movie = await getMovieWorker(context, "ru");
-  // const paths = [Array(3)].map((item) => ({
-  //   params: { id: moviesData.id.toString(), lang: "ru" },
-  // }));
+export const getStaticPaths = async () => {
+  const paths = [Array(3)].map((item) => ({
+    params: { id: moviesData.id.toString(), lang: "ru" },
+  }));
   return {
-    props: {
-      // movie,
-    },
+    paths,
+    fallback: "blocking",
   };
 };
 
